7211e07ac0f8ff1315a9830ff04a4948b77644f0
hackerFaruk <onuralparslan97@gmail.com>
2025-02-18 19:17:56 +0300

    added css

--- src/main/java/com/formsklon/demo/controller/MultiSegmentController.java
+++ src/main/java/com/formsklon/demo/controller/MultiSegmentController.java
@@ -9,7 +9,7 @@ public class MultiSegmentController {
 
     @GetMapping("/multi")
     public String home(Model model) {
-        String[] items = { "Item 1", "Item 2", "Item 3", "Item 4" };
+        String[] items = { "Soru 1", "Soru 2", "Soru 3", "Soru 4" };
         model.addAttribute("items", items); // Model'e veri ekledik
         return "multi"; // multi.html sayfasını döndürüyoruz
     }
