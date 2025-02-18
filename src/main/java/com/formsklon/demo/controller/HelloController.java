062970d74fd41460167dd996745192c70a15a773
hackerFaruk <onuralparslan97@gmail.com>
2025-02-18 21:09:33 +0300

    day1

--- src/main/java/com/formsklon/demo/controller/HelloController.java
+++ src/main/java/com/formsklon/demo/controller/HelloController.java
@@ -1,9 +1,8 @@
 package com.formsklon.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
@@ -12,5 +11,11 @@ public class HelloController {
     public String hello() {
         return "hello";
     }
+
+    @RequestMapping("/byname")
+    public String requestMethodName(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return "hello " + name;
+    }
+    // http://localhost:8080/byname?name=Deniz
 
 }
