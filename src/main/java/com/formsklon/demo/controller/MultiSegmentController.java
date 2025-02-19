2c64cfbf2a3b749becdb3f84923cddb8f225c1df
hackerFaruk <onuralparslan97@gmail.com>
2025-02-19 19:07:23 +0300

    asd

--- src/main/java/com/formsklon/demo/controller/MultiSegmentController.java
+++ src/main/java/com/formsklon/demo/controller/MultiSegmentController.java
@@ -1,16 +1,16 @@
 package com.formsklon.demo.controller;
 
-import org.springframework.stereotype.Controller; // @Controller annotation'ı ekledik.
+import org.springframework.stereotype.Controller; 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller // Sınıfı Controller olarak işaret ettik
+@Controller 
 public class MultiSegmentController {
 
     @GetMapping("/multi")
     public String home(Model model) {
         String[] items = { "Soru 1", "Soru 2", "Soru 3", "Soru 4" };
-        model.addAttribute("items", items); // Model'e veri ekledik
-        return "multi"; // multi.html sayfasını döndürüyoruz
+        model.addAttribute("items", items); // Modele veri ekledik
+        return "multi"; 
     }
 }
